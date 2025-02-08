050a9cf361aa92da0bbd88372dd4616278812258
Aiden Stone <[176074820+BlockyHead06@users.noreply.github.com]>
2025-02-08 10:01:04 -0500

    Add class to tiles

--- script.js
+++ script.js
@@ -3,5 +3,6 @@ const container = document.querySelector(".container");
 let gridSize = 16 * 16
 for (let i = 0; i < gridSize; i++) {
     let tile = document.createElement("div");
+    tile.classList.add("tile");
     container.appendChild(tile);
 }
