35339b569c47b78e95b7f9c6149893ab5839e156
Aiden Stone <176074820+aidenwstone@users.noreply.github.com>
2025-04-15 10:24:08 -0400

    Color tiles black when hovered over

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const container = document.querySelector(".container");
 
+function colorTile(e) {
+    let tile = e.target;
+    tile.style.backgroundColor = "black";
+}
+
 let gridSize = 16 * 16
 for (let i = 0; i < gridSize; i++) {
     let tile = document.createElement("div");
     tile.classList.add("tile");
     container.appendChild(tile);
+    tile.addEventListener("mouseenter", colorTile);
 }
